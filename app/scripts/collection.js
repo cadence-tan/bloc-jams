fcefb39fb52439d60225fda6a7ad930d8f577786
Cadence <cadence.tan12@gmail.com>
2015-01-31 23:18:17 +0000

    Used jQuery to populate the collection view.

--- app/scripts/collection.js
+++ app/scripts/collection.js
@@ -1,32 +1,37 @@
 var buildAlbumThumbnail = function() {
-   var template =
-       '<div class="collection-album-container col-md-2">'
-     + '  <img src="/images/album-placeholder.png"/>'
-     + '  <div class="caption album-collection-info">'
-     + '    <p>'
-     + '      <a class="album-name" href="/album.html"> Album Name </a>'
-     + '      <br/>'
-     + '      <a href="/album.html"> Artist name </a>'
-     + '      <br/>'
-     + '      X songs'
-     + '      <br/>'
-     + '    </p>'
-     + '  </div>'
-     + '</div>';
+	var template =
+	'<div class="collection-album-container col-md-2">'
+	+ '  <img src="/images/album-placeholder.png"/>'
+	+ '  <div class="caption album-collection-info">'
+	+ '    <p>'
+	+ '      <a class="album-name" href="/album.html"> Album Name </a>'
+	+ '      <br/>'
+	+ '      <a href="/album.html"> Artist name </a>'
+	+ '      <br/>'
+	+ '      X songs'
+	+ '      <br/>'
+	+ '	  	 X:XX Total Length'
+	+ '		 <br/>'
+	+ '    </p>'
+	+ '  </div>'
+	+ '</div>';
 
-  return $(template);
+	return $(template);
 };
 
 
 if (document.URL.match(/\/collection.html/)) {
   // Wait until the HTML is fully processed.
- $(document).ready(function() {
-   var $collection = $(".collection-container .row");
-   $collection.empty();
-   
-   for(var i=0; i < 33; i++){
-   	var $newThumbnail = buildAlbumThumbnail();
-   	$collection.append($newThumbnail);
-   }
- });
+  $(document).ready(function() {
+  	var $collection = $(".collection-container .row");
+  	$collection.empty();
+
+  	//Generate a random number of albums between 25 and 100
+  	var randomNum = Math.floor((Math.random() * 100) + 25);
+
+  	for(var i=0; i < randomNum ; i++){
+  		var $newThumbnail = buildAlbumThumbnail();
+  		$collection.append($newThumbnail);
+  	}
+  });
 }
