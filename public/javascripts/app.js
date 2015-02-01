5dd1912e5c906bdd2d00a0fe83b0a25255f64f99
Cadence <cadence.tan12@gmail.com>
2015-02-01 21:23:52 +0000

    Added an overlay to the collection view

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -196,7 +196,9 @@ require('./album');
 var buildAlbumThumbnail = function() {
 	var template =
 	'<div class="collection-album-container col-md-2">'
+	+ '  <div class="collection-album-image-container">'
 	+ '  <img src="/images/album-placeholder.png"/>'
+	+ '  </div>'
 	+ '  <div class="caption album-collection-info">'
 	+ '    <p>'
 	+ '      <a class="album-name" href="/album.html"> Album Name </a>'
@@ -215,20 +217,51 @@ var buildAlbumThumbnail = function() {
 };
 
 
+var buildAlbumOverlay = function(albumURL) {
+  var template =
+      '<div class="collection-album-image-overlay">'
+    + '  <div class="collection-overlay-content">'
+    + '    <a class="collection-overlay-button" href="' + albumURL + '">'
+    + '      <i class="fa fa-play"></i>'
+    + '    </a>'
+    + '    &nbsp;'
+    + '    <a class="collection-overlay-button">'
+    + '      <i class="fa fa-plus"></i>'
+    + '    </a>'
+    + '  </div>'
+    + '</div>'
+    ;
+  return $(template);
+};
+
+var updateCollectionView = function() {
+  var $collection = $(".collection-container .row");
+  //Empty entire collection, ensure there is only one in a page
+  $collection.empty();
+
+  for (var i = 0; i < 33; i++) {
+    var $newThumbnail = buildAlbumThumbnail();
+    $collection.append($newThumbnail);
+  }
+
+  var onHover = function(event) {
+     $(this).append(buildAlbumOverlay("/album.html"));
+   };
+
+   var offHover = function(event) {
+      $(this).find('.collection-album-image-overlay').remove();
+    };
+
+   $collection.find('.collection-album-image-container').hover(onHover, offHover);
+
+};
+
 if (document.URL.match(/\/collection.html/)) {
   // Wait until the HTML is fully processed.
-  $(document).ready(function() {
-  	var $collection = $(".collection-container .row");
-  	$collection.empty();
-
-  	//Generate a random number of albums between 25 and 100
-  	var randomNum = Math.floor((Math.random() * 100) + 25);
-
-  	for(var i=0; i < randomNum ; i++){
-  		var $newThumbnail = buildAlbumThumbnail();
-  		$collection.append($newThumbnail);
-  	}
-  });
+	 $(document).ready(function() {
+	     // Your code goes here.
+	 updateCollectionView();
+	});
 }
 });
 
